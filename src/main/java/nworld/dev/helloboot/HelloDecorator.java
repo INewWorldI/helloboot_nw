ebb1b033e39c035b52f40e276eba07fe9a64a844
INewWorldI <newworld-@naver.com>
2023-03-19 23:50:47 +0900

    feat: 데코레이터 패턴 적용

---
+++ src/main/java/nworld/dev/helloboot/HelloDecorator.java
@@ -0,0 +1,24 @@
+package nworld.dev.helloboot;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class HelloDecorator implements HelloService{
+    private final HelloService helloService;
+
+    public HelloDecorator(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @Override
+    public String sayHello(String name) {
+        return "*" + helloService.sayHello(name) + "*";
+    }
+
+    @Override
+    public String sayCars(String car) {
+        return "*" + helloService.sayCars(car) + "*";
+    }
+}
