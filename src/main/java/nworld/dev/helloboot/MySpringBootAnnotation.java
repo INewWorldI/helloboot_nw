3887da797422da4d865e2162fdcd103161a74496
INewWorldI <newworld-@naver.com>
2023-03-21 20:52:39 +0900

    feat: 메타 애노테이션 생성

---
+++ src/main/java/nworld/dev/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,16 @@
+package nworld.dev.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // 애노테이션 정보가 메모리에 로드 될때 까지 계속 살아있게 한다.
+@Target(ElementType.TYPE)
+@Configuration
+@ComponentScan
+public @interface MySpringBootAnnotation {
+}
