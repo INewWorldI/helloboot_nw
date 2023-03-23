d1912827e0c14b267b978cd57f1fc052e142960d
INewWorldI <newworld-@naver.com>
2023-03-23 23:22:17 +0900

    feat: Jetty 서버에 대한 구성 정보 추가

--- src/main/java/nworld/dev/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/nworld/dev/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,14 @@
 package nworld.dev.config.autoconfig;
 
 import nworld.dev.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean(name = "TomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean(name = "JettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
